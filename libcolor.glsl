6947192413d9d2b41448bfb506a129233abf2e1d
polyfloyd <floyd@polyfloyd.net>
2018-05-09 21:09:56 +0200

    Make libcolor compatible with really old GLSL versions

--- libcolor.glsl
+++ libcolor.glsl
@@ -7,24 +7,22 @@ vec3 hsvToRGB(vec3 hsv) {
 	}
 
 	float hh = mod(h, 1) * 6;
-	int i = int(hh);
-	float ff = hh - i;
+	float ff = mod(hh, 1);
 	float p = v * (1.0 - s);
 	float q = v * (1.0 - (s * ff));
 	float t = v * (1.0 - (s * (1.0 - ff)));
 
-	switch (i) {
-		case 0:
-			return vec3(v, t, p);
-		case 1:
-			return vec3(q, v, p);
-		case 2:
-			return vec3(p, v, t);
-		case 3:
-			return vec3(p, q, v);
-		case 4:
-			return vec3(t, p, v);
-		default:
-			return vec3(v, p, q);
+	if (hh <= 1) {
+		return vec3(v, t, p);
+	} else if (hh <= 2) {
+		return vec3(q, v, p);
+	} else if (hh <= 3) {
+		return vec3(p, v, t);
+	} else if (hh <= 4) {
+		return vec3(p, q, v);
+	} else if (hh <= 5) {
+		return vec3(t, p, v);
+	} else {
+		return vec3(v, p, q);
 	}
 }
