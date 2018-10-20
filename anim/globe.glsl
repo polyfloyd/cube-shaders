42dad5d5f6a9481e4984ac27a52c96b40a393076
polyfloyd <floyd@polyfloyd.net>
2018-10-20 16:41:29 +0200

    globe: Fix orientation and rotation for non-gyrosensor renders

--- anim/globe.glsl
+++ anim/globe.glsl
@@ -5,10 +5,10 @@
 
 void mainCube(out vec4 fragColor, in vec3 fragCoord) {
 	fragCoord *= mat3(gyros);
-	float tLight = iTime * 1.2;
+	float tLight = iTime * -1.2;
 	float tGeo = iTime * 0.1;
 
-	vec2 uv = map_to_sphere_uv(fragCoord);
+	vec2 uv = map_to_sphere_uv(fragCoord) * vec2(-1, 1);
 	vec4 li = texture2D(lightTex, vec2(-uv.x - tGeo, uv.y));
 	vec4 da = texture2D(darkTex, vec2(-uv.x - tGeo, uv.y));
 
